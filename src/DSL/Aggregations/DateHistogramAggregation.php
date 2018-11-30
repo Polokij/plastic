cda97a2c7edfa99f77a2d343415a266676589980
Vitaliy Lyubimov <lubimov@ukr.net>
2018-11-30 09:40:52 +0100

    Implemented the method - flatten result on date histogram aggregation class

--- src/DSL/Aggregations/DateHistogramAggregation.php
+++ src/DSL/Aggregations/DateHistogramAggregation.php
@@ -11,6 +11,8 @@ namespace Sleimanx2\Plastic\DSL\Aggregations;
 
 use ONGR\ElasticsearchDSL\Aggregation\AbstractAggregation;
 use ONGR\ElasticsearchDSL\Aggregation\Bucketing\DateHistogramAggregation as DateHistogram;
+use ONGR\ElasticsearchDSL\Aggregation\Metric\SumAggregation;
+use ONGR\ElasticsearchDSL\Aggregation\Pipeline\BucketScriptAggregation;
 
 
 /**
@@ -21,6 +23,37 @@ use ONGR\ElasticsearchDSL\Aggregation\Bucketing\DateHistogramAggregation as Date
 class DateHistogramAggregation extends DateHistogram
 {
 
+    public function flattenResult($result){
+        $termResults = $result[$this->getName()] ?? null;
 
+        if( ! $termResults || ! isset($termResults['buckets'])){
+            return [];
+        }
+        $aggregations = collect($this->getAggregations())
+            ->filter(function($agg){
+                // skip the bucket sort aggregation because it doesn't have the result value in the buckets
+                return ! $agg instanceof BucketSortAggregation;
+            });
+
+        // Mapping the bucket to have a flat result similar
+        $buckets = collect($termResults['buckets'])
+            // iterate the buckets
+            ->map(function($bucket) use ($aggregations){
+
+                // iterating the bucket fields
+                $bucketResult = $aggregations->mapWithKeys(function(AbstractAggregation $aggr) use ($bucket){
+                    $fieldName = $aggr->getName();
+                    if($aggr instanceof SumAggregation || $aggr instanceof BucketScriptAggregation){
+                        return [$fieldName => $bucket[$fieldName]['value'] ?? 0];
+                    }
+                    return $aggr->flattenResult($bucket);
+                });
+                $bucketResult[$this->getName()] = $bucket['key'];
+                return $bucketResult;
+            });
+
+        return $buckets;
+
+    }
 
 }
